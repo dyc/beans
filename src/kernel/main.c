b3f51d29163e6ae96164fa6bebfde205ade4f925
dennis <dennischengo@gmail.com>
2020-08-23 14:54:55 -0500

    colors

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,7 +4,12 @@ void kmain(void) {
   fb_init();
 
   for (size_t i = 0; i < FB_HEIGHT; i++) {
+    fb_fg(FB_COLOR_MAGENTA);
+    fb_bg(FB_COLOR_BROWN);
     fb_writestring("hello\n");
+
+    fb_fg(FB_COLOR_CYAN);
+    fb_bg(FB_COLOR_GREEN);
     fb_writestring("world\n");
   }
 }
