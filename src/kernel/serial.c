f0364e2b225512e158f017cc3815a28cabfecd57
dennis <dennischengo@gmail.com>
2020-08-26 09:31:18 -0500

    serial nits

--- src/kernel/serial.c
+++ src/kernel/serial.c
@@ -7,7 +7,7 @@
 #include <sys/io.h>
 
 static uint8_t ENABLE_SET_DIVISOR = 0x80;
-// (with divisor mode disabled) set data length to 8 bits,
+// (disable divisor mode) set data length to 8 bits,
 // 1 stop bit, no parity bit, break control disabled
 static uint8_t DEFAULT_LINE_CONFIG = 0x03;
 // enable 14 byte FIFO and clear it
@@ -17,49 +17,57 @@ static uint8_t DEFAULT_FIFO_CONFIG = 0xC7;
 static uint8_t DEFAULT_MODEM_CONFIG = 0x03;
 static uint8_t TRANSMIT_FIFO_EMPTY = 0x20;
 
-static inline short int data_port(enum serial_port p) {
+static inline short int data_reg(enum serial_port p) {
   return p;
 }
 
-static inline short int fifo_port(enum serial_port p) {
+static inline short intenable_reg(enum serial_port p) {
+  return p + 1;
+}
+
+static inline short int fifocontrol_reg(enum serial_port p) {
   return p + 2;
 }
 
-static inline short int line_port(enum serial_port p) {
+static inline short int linecontrol_reg(enum serial_port p) {
   return p + 3;
 }
 
-static inline short int modem_port(enum serial_port p) {
+static inline short int modemcontrol_reg(enum serial_port p) {
   return p + 4;
 }
 
-static inline short int status_port(enum serial_port p) {
+static inline short int linestatus_reg(enum serial_port p) {
   return p + 5;
 }
 
 bool transmit_empty(enum serial_port p) {
-  return inb(status_port(p)) & TRANSMIT_FIFO_EMPTY;
+  return inb(linestatus_reg(p)) & TRANSMIT_FIFO_EMPTY;
 }
 
 void transmit(enum serial_port p, char c) {
   while (!transmit_empty(p));
-  outb(data_port(p), c);
+  outb(data_reg(p), c);
 }
 
 void serial_enable(enum serial_port p) {
+  outb(intenable_reg(p), 0x00);
   // 115200 bps
   serial_set_baud(p, 0x01);
-  outb(line_port(p), DEFAULT_LINE_CONFIG);
-  outb(fifo_port(p), DEFAULT_FIFO_CONFIG);
-  outb(modem_port(p), DEFAULT_MODEM_CONFIG);
+  outb(linecontrol_reg(p), DEFAULT_LINE_CONFIG);
+  outb(fifocontrol_reg(p), DEFAULT_FIFO_CONFIG);
+  outb(modemcontrol_reg(p), DEFAULT_MODEM_CONFIG);
+  outb(intenable_reg(p), 0x01);
 }
 
 void serial_set_baud(enum serial_port p, unsigned short divisor) {
-  uint8_t prev = inb(line_port(p));
-  outb(line_port(p), ENABLE_SET_DIVISOR);
-  outb(data_port(p), (divisor >> 8) & 0x00FF);
-  outb(data_port(p), divisor & 0x00FF);
-  outb(line_port(p), prev);
+  uint8_t prevline = inb(linecontrol_reg(p));
+  outb(linecontrol_reg(p), ENABLE_SET_DIVISOR);
+  outb(data_reg(p), (divisor >> 8) & 0xFF);
+  outb(data_reg(p), divisor & 0xFF);
+  // seems like folks usually write something like
+  // DEFAULT_LINE_CONFIG in lieu of prev here
+  outb(linecontrol_reg(p), prevline);
 }
 
 void serial_write(enum serial_port p, const char* s) {
