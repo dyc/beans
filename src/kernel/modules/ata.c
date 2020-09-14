44ca315fcddbfe9587343779430b27ae136b12c0
dennis <dennischengo@gmail.com>
2020-09-14 08:59:16 -0500

    format

--- src/kernel/modules/ata.c
+++ src/kernel/modules/ata.c
@@ -1,10 +1,6 @@
 #include <sys/io.h>
 
 static int detect_device() __attribute__((unused));
-static int detect_device() {
-  return 0;
-}
+static int detect_device() { return 0; }
 
-void ata_init() {
-  return;
-}
+void ata_init() { return; }
