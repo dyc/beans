e98ab95fecc608f45f6dfc0d25fdbac2a5604c2f
dennis <dennischengo@gmail.com>
2020-09-15 20:50:04 -0500

    load and jump to...ourself?
    but in a diff code segment. prob worth taking a closer look at this one

--- host/isatty.c
+++ host/isatty.c
@@ -1,6 +1,9 @@
 #include <dlfcn.h>
 #include <unistd.h>
 
+int _isatty(int fd) { return 1; }
+
+#ifndef DYLD_INTERPOSE
 #define DYLD_INTERPOSE(_replacment, _replacee)                                 \
   __attribute__((used)) static struct {                                        \
     const void *replacment;                                                    \
@@ -8,7 +11,6 @@
   } _interpose_##_replacee __attribute__((section("__DATA,__interpose"))) = {  \
       (const void *)(unsigned long)&_replacment,                               \
       (const void *)(unsigned long)&_replacee};
-
-int _isatty(int fd) { return 1; }
-
 DYLD_INTERPOSE(_isatty, isatty);
+#undef DYLD_INTERPOSE
+#endif
