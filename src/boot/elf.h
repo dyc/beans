285a942e569813a42ce8650438a0b957e63d172d
dennis <dennischengo@gmail.com>
2020-09-27 20:31:40 -0500

    more elfh parsing and put loadk in its own prelude section

--- src/boot/elf.h
+++ src/boot/elf.h
@@ -2,12 +2,28 @@
 
 #include <stdint.h>
 
-#define ELFI_N 16
-#define ELFI_MAGIC0 0x7f
-#define ELFI_MAGIC1 'E'
-#define ELFI_MAGIC2 'L'
-#define ELFI_MAGIC3 'F'
+#define ELF_IDENT_N 16
+#define ELF_IDENT_MAGIC0 0x7f
+#define ELF_IDENT_MAGIC1 'E'
+#define ELF_IDENT_MAGIC2 'L'
+#define ELF_IDENT_MAGIC3 'F'
+#define ELF_IDENT_32BIT 1
+#define ELF_IDENT_L_ENDIAN 1
+#define ELF_ISA_X86 3
 
 struct elf_header {
-  uint8_t ident[ELFI_N];
+  uint8_t ident[ELF_IDENT_N];
+  uint16_t type;
+  uint16_t isa;
+  uint32_t version;
+  uint32_t entry;
+  uint32_t ph_offset_bytes;
+  uint32_t sh_offset_bytes;
+  uint32_t flags;
+  uint16_t header_size_bytes;
+  uint16_t ph_ent_size_bytes;
+  uint16_t ph_ents;
+  uint16_t sh_ent_size_bytes;
+  uint16_t sh_ents;
+  uint16_t sh_strtab_idx;
 } __attribute__((packed));
--- src/boot/loadk.c
+++ src/boot/loadk.c
@@ -1,12 +1,19 @@
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #include "elf.h"
 
+#define ERROR_HANG error(__LINE__);
+
 static const uint16_t *VGA_TEXT_START = (uint16_t *)0xB8000;
 const size_t VGA_WIDTH = 80;
 const size_t VGA_HEIGHT = 25;
 static uint16_t *buffer;
+struct mmap_entry {
+  uint64_t base, length;
+  uint32_t type, unused;
+} __attribute__((packed));
 
 static inline uint16_t vga_cell(uint8_t uc) {
   return (uint16_t)uc | (uint16_t)((15 | (0 << 4)) << 8);
@@ -14,11 +21,86 @@ static inline uint16_t vga_cell(uint8_t uc) {
 
 static inline size_t buffer_i(size_t x, size_t y) { return y * VGA_WIDTH + x; }
 
-struct mmap_entry {
-  uint64_t base, length;
-  uint32_t type, unused;
-} __attribute__((packed));
+static size_t decwidth(int value) {
+  if (value < 0) {
+    value *= -1;
+  }
 
+  size_t width = 0;
+  do {
+    ++width;
+    value = value / 10;
+  } while (value > 0);
+  return width;
+}
+
+static int printdec(char *out, int value) {
+  char *stop = out;
+  if (value < 0) {
+    *stop = '-';
+    ++stop;
+  }
+
+  char *start = stop + decwidth(value) - 1;
+  for (char *o = start; o >= stop; --o) {
+    *o = (value % 10) + '0';
+    value /= 10;
+  }
+  return start - out + 1;
+}
+
+static int sprintf(char *out, const char *fmt, ...) {
+  va_list arg_p;
+  va_start(arg_p, fmt);
+
+  char *o = out;
+  for (const char *f = fmt; *f;) {
+    if (*f != '%') {
+      *o = *f;
+      ++o;
+      ++f;
+      continue;
+    }
+
+    ++f;
+    switch (*f) {
+    case 'd': {
+      o += printdec(o, va_arg(arg_p, int));
+      ++f;
+      break;
+    }
+    default: {
+      break;
+    }
+    }
+  }
+  va_end(arg_p);
+
+  *o = '\0';
+  return o - out;
+}
+
+static void error(size_t line_num) {
+  char buf[8];
+  sprintf(buf, "err: %d", line_num);
+  for (size_t i = 0; i < sizeof(buf); ++i) {
+    buffer[buffer_i(i, 0)] = vga_cell(buf[i]);
+  }
+  while (1)
+    ;
+}
+
+static void kmain() {
+  buffer[buffer_i(0, 0)] = vga_cell(':');
+  buffer[buffer_i(1, 0)] = vga_cell('-');
+  buffer[buffer_i(2, 0)] = vga_cell(')');
+  while (1)
+    ;
+}
+
+// todo: ...is there another way to do this?
+void loadk(size_t mmap_entries, struct mmap_entry *mmap, uint32_t *kernel,
+           uint32_t *initrd) __attribute__((section(".text.loadk")));
 void loadk(size_t mmap_entries, struct mmap_entry *mmap, uint32_t *kernel,
            uint32_t *initrd) {
   (void)mmap_entries;
@@ -31,15 +113,23 @@ void loadk(size_t mmap_entries, struct mmap_entry *mmap, uint32_t *kernel,
     }
   }
 
-  buffer[buffer_i(0, 0)] = vga_cell(':');
-  buffer[buffer_i(1, 0)] = vga_cell('-');
   struct elf_header *kernel_elf = (struct elf_header *)kernel;
-  if (kernel_elf->ident[0] != ELFI_MAGIC0 ||
-      kernel_elf->ident[1] != ELFI_MAGIC1 ||
-      kernel_elf->ident[2] != ELFI_MAGIC2 ||
-      kernel_elf->ident[3] != ELFI_MAGIC3) {
-    buffer[buffer_i(2, 0)] = vga_cell('(');
-  } else {
-    buffer[buffer_i(2, 0)] = vga_cell(')');
+  if (kernel_elf->ident[0] != ELF_IDENT_MAGIC0 ||
+      kernel_elf->ident[1] != ELF_IDENT_MAGIC1 ||
+      kernel_elf->ident[2] != ELF_IDENT_MAGIC2 ||
+      kernel_elf->ident[3] != ELF_IDENT_MAGIC3) {
+    ERROR_HANG;
   }
+
+  if (kernel_elf->ident[4] != ELF_IDENT_32BIT) {
+    ERROR_HANG;
+  }
+  if (kernel_elf->ident[5] != ELF_IDENT_L_ENDIAN) {
+    ERROR_HANG;
+  }
+  if (kernel_elf->isa != ELF_ISA_X86) {
+    ERROR_HANG;
+  }
+
+  kmain();
 }
